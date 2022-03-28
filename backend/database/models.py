c6a3f9ea0d40161f8b2925e8002b6f76930861b4
周羿 <921850443@qq.com>
2022-03-28 14:10:49 +0800

    info interface

--- backend/database/models.py
+++ backend/database/models.py
@@ -8,6 +8,75 @@
 from django.db import models
 
 
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
 class ConDirRecord(models.Model):
     con_dir_id = models.IntegerField(blank=True, null=True)
     con_id = models.IntegerField(blank=True, null=True)
@@ -54,7 +123,7 @@ class Consultant(models.Model):
     password = models.CharField(max_length=254, blank=True, null=True)
     unit = models.CharField(max_length=254, blank=True, null=True)
     title = models.CharField(max_length=254, blank=True, null=True)
-    icon = models.TextField(blank=True, null=True)
+    icon = models.CharField(max_length=254, blank=True, null=True)
     av_score = models.FloatField(blank=True, null=True)
     totel_num = models.IntegerField(blank=True, null=True)
     totel_dur = models.BigIntegerField(blank=True, null=True)
@@ -99,7 +168,7 @@ class Director(models.Model):
     title = models.CharField(max_length=254, blank=True, null=True)
     qual = models.CharField(max_length=254, blank=True, null=True)
     qualnum = models.CharField(max_length=254, blank=True, null=True)
-    icon = models.TextField(blank=True, null=True)
+    icon = models.CharField(max_length=254, blank=True, null=True)
     number = models.IntegerField(blank=True, null=True)
     duration = models.BigIntegerField(blank=True, null=True)
     u_ticket = models.CharField(max_length=254, blank=True, null=True)
@@ -109,6 +178,51 @@ class Director(models.Model):
         db_table = 'director'
 
 
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
 class Visitor(models.Model):
     vis_id = models.AutoField(primary_key=True)
     password = models.CharField(max_length=254, blank=True, null=True)
@@ -116,9 +230,9 @@ class Visitor(models.Model):
     tele = models.CharField(max_length=254, blank=True, null=True)
     emer_name = models.CharField(max_length=254, blank=True, null=True)
     emer_tele = models.CharField(max_length=254, blank=True, null=True)
-    icon = models.TextField(blank=True, null=True)
     ban = models.IntegerField(blank=True, null=True)
     type = models.IntegerField(blank=True, null=True)
+    icon = models.CharField(max_length=254, blank=True, null=True)
     u_ticket = models.CharField(max_length=254, blank=True, null=True)
 
     class Meta:
