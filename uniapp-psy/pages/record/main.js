f4c93b94e8294ce3b6718a85a90480c46a19272a
chenyyyy-0824 <1134084746@qq.com>
2022-04-18 19:55:03 +0800

    获取后端的历史咨询
    uniapp获取后端历史记录

--- uniapp-psy/pages/record/main.js
+++ uniapp-psy/pages/record/main.js
@@ -1,4 +1,6 @@
 // pages/record/main.js
+const app = getApp()
+
 Page({
 
     /**
@@ -6,73 +8,31 @@ Page({
      */
     data: {
         "type": " ",
-        historyConversation1:[],
-        historyConversation: [
-            {
-                startTime:"04-08 16:23",
-                status:"",
-                counselor:{
-                    avatar: "https://i03piccdn.sogoucdn.com/0354d6ffc3ecbe11",
-                    name:"王咨询师"
-                },
-                duration:"6:23",
-                evaluate:3, 
-            },
-            {
-                startTime:"04-08 16:22",
-                status:"",
-                counselor:{
-                    avatar: "https://i03piccdn.sogoucdn.com/0354d6ffc3ecbe11",
-                    name:"王咨询师"
-                },
-                duration:"4:23",
-                evaluate:4,
-            },
-            {
-                startTime:"04-07 15:21",
-                status:"",
-                counselor:{
-                    avatar: "https://i03piccdn.sogoucdn.com/0354d6ffc3ecbe11",
-                    name:"王咨询师"
-                },
-                duration:"3:23",
-                evaluate:1,
-            }
-        ]
+        historyConversation: [],
     },
-    // onLoad: function(){
-    //     var that = this;
-    //     header = {'cookie':wx.getStorageSync("token")};
-    //     wx.request({
-    //       url: 'url',
-    //       method: "GET",
-    //       success: function(res){
-    //           console.log("咨询记录 res:"+res);
-    //           that.setData(
-    //             {
-    //                 historyConversation1: res.data.xxx   根据具体的res.data内容赋值给historyConversation1
-    //             }
-    //           )
-    //       },
-    //     fail: function() {
-    //       console.log('咨询记录获取失败');
-    //       }
-    //     })
-    // },
+    onLoad: function(){
+        var that = this;
+        //header = {'cookie':wx.getStorageSync("token")};
+        var cookienow = app.globalData.mycookie;
+        console.log(cookienow)
+        wx.request({
+          url: 'http://123.57.45.27:8022/visitor/weChat/conversation/',
+          method: "GET",
+          header : {'cookie': cookienow},
+          success: function(res){
+              console.log("res:"+ res.data.historyConversation[0].evaluate);
+              console.log("res:"+ res.data.historyConversation[0].consultants.name);
+              that.setData(
+                {
+                    historyConversation: res.data.historyConversation
+                }
+              )
+              console.log("history:"+that.data.historyConversation[0].evaluate);
+          },
+        fail: function() {
+          console.log('咨询记录获取失败');
+          }
+        })
+    }
 
-    onShow: function(){
-        let model = getApp().globalData.model
-        if(model){
-          console.log("历史记录：", model)
-          var items = JSON.parse(model);
-          console.log("历史记录：",items)
-          var self = this
-          self.setData({
-            historyConversation:items,
-          });
-        }
-        else{
-          console.log("没有历史记录")
-        }
-      }
-})
+ })
